de5deddd86a9c3b9447281dd841dcb409d189b43
QDex Maintainer <qdex-maintainer@users.noreply.github.com>
2026-05-25 14:27:29 +0200

    Hide console for Tauri release build

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 use base64::{engine::general_purpose, Engine as _};
 use edge_tts_rust::{Boundary, EdgeTtsClient, SpeakOptions};
 use serde::Serialize;
